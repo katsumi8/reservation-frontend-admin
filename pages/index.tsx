24ce9fa0a336e97fb33e1e6e121aec9e5a9044e3
katsumi8 <katsumi.ishihara08@gmail.com>
2022-11-21 16:17:16 +0100

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function Home() {
   type clickedCellType = {
@@ -11,11 +11,8 @@ function Home() {
   const cellOnClickHandler = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    let newElement: clickedCellType = { id: e.currentTarget.id };
-
-    // if it exists already, delete
-
-    setClickedData((prev) => [...prev, newElement]);
+    // let newElement: clickedCellType = { id: e.currentTarget.id };
+    // setClickedData((prev) => [...prev, newElement]);
     console.log(clickedData);
   };
 
@@ -23,7 +20,7 @@ function Home() {
   for (let i = 0; i < 15 * 15; i++) {
     list.push(
       <div
-        className="aspect-square rounded-lg border-blue-500 bg-slate-50 p-4 shadow-lg"
+        className="aspect-square rounded-lg border-blue-500 bg-slate-50 p-1 shadow-lg"
         key={i.toString()}
         id={i.toString()}
         onClick={cellOnClickHandler}
@@ -32,32 +29,22 @@ function Home() {
       </div>
     );
   }
-  // list.push(
-  //   <div className="absolute col-start-3 row-start-4 col-end-6 aspect-square rounded-lg border-blue-500 bg-red-50 p-4 shadow-lg">
-  //     test
-  //   </div>
-  // );
 
   return (
-
-    <div className="container absolute m-2 mx-auto overflow-auto rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
-      <section className="absolute m-auto grid aspect-square grid-cols-15 gap-1 rounded-lg text-center font-mono text-sm font-bold leading-6 text-slate-500">
+    <div className="container absolute mx-auto overflow-scroll rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+      <section className="absolute grid aspect-square w-[80vw] grid-cols-15 grid-rows-15 gap-1 rounded-lg text-center font-mono text-sm font-bold leading-6 text-slate-500">
         {list}
       </section>
-      <section className="relative m-auto grid aspect-square grid-cols-15 gap-1 rounded-lg text-center font-mono text-sm font-bold leading-6 text-slate-500">
-        <div className="col-start-3 col-end-5 aspect-square rounded-lg border-blue-500 bg-red-50 p-4">
+      <section className="relative grid aspect-square w-[80vw] grid-cols-15 grid-rows-15 gap-1 rounded-lg text-center font-mono text-sm font-bold leading-6 text-slate-500">
+        <div
+          className="col-start-3 col-end-5 row-start-3 row-end-5 aspect-square rounded-lg border-blue-500 bg-red-50 p-1"
+          onClick={cellOnClickHandler}
+        >
           test
         </div>
       </section>
+
     </div>
-
-
-    // <div className="container absolute m-2 mx-auto overflow-auto rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
-    // <div>
-    //   <section className="relative m-auto grid aspect-square grid-cols-15 grid-rows-15 gap-1 rounded-lg text-center font-mono text-sm font-bold leading-6 text-slate-500">
-    //     {list}
-    //   </section>
-    // </div>
   );
 }
 
