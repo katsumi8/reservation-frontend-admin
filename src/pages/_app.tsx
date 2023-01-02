52fd5da18cbc649991fcedb642c906892fced28a
katsu8 <katsumi.ishihara08@gmail.com>
2023-01-02 10:37:37 +0100

    made a backend url to env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import "tailwindcss/tailwind.css";
 import "../../styles/global.css";
 
 const client = new ApolloClient({
-  uri: "https://reservation-system.onrender.com/",
-  // uri: process.env.NEXT_PUBLIC_BACKEND_URL as string,
+  // uri: "https://reservation-system.onrender.com/",
+  uri: process.env.NEXT_PUBLIC_BACKEND_URL as string,
   cache: new InMemoryCache(),
 });
 
